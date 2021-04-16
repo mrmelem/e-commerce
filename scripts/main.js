23a3c0a73e33eb5aa104b2fe95858d7c50376ddf
Pedro Melém <Melemredes@gmail.com>
2021-04-16 07:18:14 -0300

    1.1.0
    Cadastro de novos produtos via arquivo seeds/catalogo.txt com Javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,11 @@
 $(() => {
 
-    var slideImages = ['slide1.jpeg', 'slide2.jpeg', 'slide3.jpeg', 'slide4.jpeg', 'slide5.jpeg', 'slide6.jpeg', 'slide7.jpeg']
-
-    box = (image) => {
-        return `
-        <div class="box">
-          <div class="image">
-            <img src="./assets/${image}" alt="">
-          </div>
-        </div>
-        `
-    }
-
-    var width = 180
-    var index = 0;
-    var click = false;
-    var left = 20
-
-    $('#previous').hide()
-
-    for (var i = 0; i < slideImages.length; i++) {
-        render(slideImages[i])
-    }
-
+    var matches = '';
+    var campos = []
+    var h, i, x, dados = []
+    var card = ''
+    var info;
+    var cardInfo;
 
     // Clicks
     $('.btn .btn-nav').click(function () {
@@ -35,51 +18,67 @@ $(() => {
         $(this).siblings('.body').slideToggle(500)
     })
 
-    setInterval(function () {
-        if (click) {
-            click = false
-        } else {
-            slide('next')
+    async function getBox() {
+        await $.get('../components/box.html', data => {
+            regexp = RegExp('{{!(.*?)}}*', 'g')
+            matches = data.matchAll(regexp)
+            card = data
+            for (match of matches) {
+                h++
+                campos[match[1]] = match[0]
+            }
+        })
+
+        await $.get('../seeds/catalogo', data => {
+            var lista = data.split('\n')
+            for (i = 0; i < lista.length; i++) {
+                if (lista[i] == '' || lista[i] == ' ') continue
+
+                var fields = lista[i].split(', ')
+
+                dados[i] = []
+                for (x = 0; x < fields.length; x++) {
+                    info = fields[x].split(': ')
+                    dados[i][info[0]] = info[1]
+                }
+            }
+        })
+
+        for (let cont = 0; cont < (dados.length - 1); cont++) {
+            var render = card;
+
+            render = render.replace(campos.Produto, dados[cont]['Produto'])
+            render = render.replace(campos.Valor, dados[cont]['Valor'])
+            render = render.replace(campos.Imagem, dados[cont]['Imagem'])
+
+            $('.catalogo .body .container').append(render)
+
+
         }
-    }, 2000)
-
-
-    function render(image) {
-        $('.slide').append(box(image))
-    }
-    function slide(direction) {
-
-        if (direction == 'previous') {
-            left = left + width + 10
-            index--;
-        } else {
-            index++;
-            left = left - width - 10
-        }
-        if (left > (i - 5) * 190 || left < (i - 5) * -190) {
-            left = 20;
-            index = 0;
-        }
-        if (($('.slide').width() + left) < 1080) {
-            left = left + 90
-        }
-
-        if (index == 0)
-            left = 20;
-
-        $('.slide').css('left', left + 'px')
-
-        if (index != 0)
-            $('#previous').show()
-        else
-            $('#previous').hide()
-
-        if (index == (i - 5))
-            $('#next').hide()
-        else
-            $('#next').show()
-
 
     }
+
+
+
+    getBox()
+
+
+    /*
+    for (x = 0; x < 10; x++) {
+        dados[x] = []
+        for (y = 0; y < 10; y++){
+            dados[x][y] = "Ok"
+        }
+    }
+
+*/
+
+
+
+
+
+
+
+
 
 })
