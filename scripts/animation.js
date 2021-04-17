674993902bfe7002897102b3f249bd3ba38790a0
Pedro Melém <Melemredes@gmail.com>
2021-04-17 04:30:34 -0300

    2.1.0
    Zoom image e Breakpoints

---
+++ scripts/animation.js
@@ -0,0 +1,19 @@
+$(document).on('click', '.box', function () {
+    let image = $(this).children('.image').children('img').attr('src')
+    let title = $(this).children('.description').children('.title').html()
+    let price = $(this).children('.description').children('.price').html()
+
+    var box = `
+        <div class="image">
+            <img src="${image}" alt="">
+        </div>
+    `
+    $('.box-focus').html(box)
+    $('.focus').css('display','flex')
+})
+
+
+
+$('.overlay').click(function () {
+    $('.focus').css('display','none')
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,70 +1,64 @@
-$(() => {
-
-    var matches,card = '';
-    var campos = []
-    var h, i, x, dados = []
-    var info;
 
 
-    async function getBox() {
-        await $.get('components/box.html', data => {
-            regexp = RegExp('{{!(.*?)}}*', 'g')
-            matches = data.matchAll(regexp)
-            card = data
-            for (match of matches) {
-                h++
-                campos[match[1]] = match[0]
-            }
-        })
-
-        await $.get('seeds/catalogo.txt', data => {
-            var lista = data.split('\n')
-            for (i = 0; i < lista.length; i++) {
-                if (lista[i] == '' || lista[i] == ' ') continue
-
-                var fields = lista[i].split(', ')
-
-                dados[i] = []
-                for (x = 0; x < fields.length; x++) {
-                    info = fields[x].split(': ')
-                    dados[i][info[0]] = info[1]
-                }
-            }
-        })
-
-        for (let cont = 0; cont < dados.length; cont++) {
-            var render = card;
-
-            render = render.replace(campos.Produto, dados[cont]['Produto'])
-            render = render.replace(campos.Valor, dados[cont]['Valor'])
-            render = render.replace(campos.Imagem, dados[cont]['Imagem'])
-
-            $('.catalogo .body .container').append(render)
+var matches, card = '';
+var campos = []
+var h, i, x, dados = []
+var info;
 
 
+async function getBox() {
+    await $.get('components/box.html', data => {
+        regexp = RegExp('{{!(.*?)}}*', 'g')
+        matches = data.matchAll(regexp)
+        card = data
+        for (match of matches) {
+            h++
+            campos[match[1]] = match[0]
         }
+    })
+
+    await $.get('seeds/catalogo.txt', data => {
+        var lista = data.split('\n')
+        for (i = 0; i < lista.length; i++) {
+            if (lista[i] == '' || lista[i] == ' ') continue
+
+            var fields = lista[i].split(', ')
+
+            dados[i] = []
+            for (x = 0; x < fields.length; x++) {
+                info = fields[x].split(': ')
+                dados[i][info[0]] = info[1]
+            }
+        }
+    })
+
+    for (let cont = 0; cont < dados.length; cont++) {
+        var render = card;
+
+        render = render.replace(campos.Produto, dados[cont]['Produto'])
+        render = render.replace(campos.Valor, dados[cont]['Valor'])
+        render = render.replace(campos.Imagem, dados[cont]['Imagem'])
+
+        $('.catalogo .body .container').append(render)
+
 
     }
 
-    getBox()
+}
+
+getBox()
 
 
-    /*
-    for (x = 0; x < 10; x++) {
-        dados[x] = []
-        for (y = 0; y < 10; y++){
-            dados[x][y] = "Ok"
-        }
+/*
+for (x = 0; x < 10; x++) {
+    dados[x] = []
+    for (y = 0; y < 10; y++){
+        dados[x][y] = "Ok"
     }
+}
 
 */
 
 
 
 
-
-
-
-
-
-})
